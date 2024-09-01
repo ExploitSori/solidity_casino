790530aadf156d49369cebfabab9963f54379bd0
Sori <ljgggg1234@gmail.com>
2024-09-02 02:26:42 +0900

    interface define

--- src/Casino.sol
+++ src/Casino.sol
@@ -7,9 +7,37 @@ import {ERC1967Utils} from "../lib/openzeppelin-contracts/contracts/proxy/ERC196
 contract Casino{
 	address owner;
 	bytes32 internal constant _IMPLEMENTATION_SLOT = keccak256("sori.implementation");
+	mapping(address=>uint) insertedMoney;
+	struct game{
+		bytes32 id;
+		uint256 userCnt;
+		address[] users;
+		uint256 money;
+		uint totalMoney;
+		uint256 maxUser;
+	}
 	modifier ownerChk{
 		require(owner == msg.sender, "not owner");
 		_;
+	}
+	function insertToken() external{
+		//approve chk => transferFrom
+	}
+	function gameJoin() external{
+		//game chk 
+		// joined game
+	}
+	function makeGame() external{
+		//makeGame
+	}
+	function claim() external {
+		//transfer
+	}
+	function games() external {
+		//print open Games
+	}
+	function totalGames() external{
+
 	}
 	function upgradeTo(address impl) internal {
 		StorageSlot.getAddressSlot(_IMPLEMENTATION_SLOT).value = impl;
