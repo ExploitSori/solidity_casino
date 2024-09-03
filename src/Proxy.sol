52ad6600563bb35a5c1f54ce4f5c13f8f04d779c
Sori <ljgggg1234@gmail.com>
2024-09-03 10:34:50 +0900

    proxy console.log delete

--- src/Proxy.sol
+++ src/Proxy.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.13;
 import {StorageSlot} from "../lib/openzeppelin-contracts/contracts/utils/StorageSlot.sol";
 import {ERC1967Utils} from "../lib/openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Utils.sol";
-import {console} from "forge-std/console.sol";
 contract Proxy{
 	bytes32 internal constant _IMPLEMENTATION_SLOT = keccak256("sori.implementation");
 	bytes32 internal constant _ADMIN_SLOT = keccak256("sori.admin");
@@ -18,8 +17,6 @@ contract Proxy{
 	fallback(bytes calldata data) external payable returns(bytes memory ret){
 		//delegatecall
 		address impl = getAddress();
-		console.log(impl);
-//		console.logBytes32(data);
 		(bool success, bytes memory res) = impl.delegatecall(data);
 		require(success, "execution fail");
 		return res;
